4333b135c76190c9eb5ae13d4b6d22bed095fb1b
unciafidelis <unciafidelis@gmail.com>
2022-05-08 10:24:38 -0500

    Add: table MissionCommander data created

--- prisma/seed.js
+++ prisma/seed.js
@@ -44,6 +44,55 @@ const prisma = new PrismaClient();
     });
 
     console.log('Create 3 explorers');
+  
+    const MC = await prisma.MissionCommander.upsert({
+        where: { name: 'MissionCommander' },
+        update: {},
+        create: {
+          name: 'MissionCommander',
+                lang: 'Woopa',
+                missionCommander: 'MC',
+                enrollments: 0,
+                hasCertification: false
+        },
+      });
+      const MC1 = await prisma.MissionCommander.upsert({
+        where: { name: 'MissionCommander1' },
+        update: {},
+        create: {
+                name: 'MissionCommander1',
+                lang: 'WoopanoParlante',
+                missionCommander: 'Carlo',
+                enrollments: 3,
+                hasCertification: true
+
+        },
+      });
+  
+      const MC2 = await prisma.MissionCommander.upsert({
+        where: { name: 'MissionCommander2' },
+        update: {},
+        create: {
+            name: 'MissionCommander2',
+            lang: 'twitcher',
+            missionCommander: 'Fer',
+            enrollments: 2,
+            hasCertification: true
+        },
+      });
+  
+      const MC3 = await prisma.MissionCommander.upsert({
+        where: { name: 'MissionCommander3' },
+        update: {},
+        create: {
+            name: 'MissionCommander3',
+            lang: 'Dobelerio',
+            missionCommander: 'Romarpla',
+            enrollments: 1,
+            hasCertification: true
+        },
+      });
+      console.log('Create 3 Mission Commanders');
   } catch(e) {
     console.error(e);
     process.exit(1);
